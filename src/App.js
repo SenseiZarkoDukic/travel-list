4eaf60e757716faf53840d691ef0a5bfab3cfa9b
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-12 17:10:13 +0200

    All components filled with content and added to App function so it is rendered.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ export default function App() {
   return (
     <div>
       <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
@@ -10,8 +13,22 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
 
-function PackingList() {}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
 
-function Stats() {}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
