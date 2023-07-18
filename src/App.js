430eaebf9862791f3c4ba28d6c3da4ed13a9dd8c
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-18 19:22:53 +0200

    Cleaning the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
