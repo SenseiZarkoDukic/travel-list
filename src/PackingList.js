0b61e90889a81d3bb0b56a34496eb88b4380651a
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-20 21:28:13 +0200

    PackingItem component created as file, and imported to the parent App component, also imported useState from 'react'

---
+++ src/PackingList.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearItems,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+  // if (sortBy === "clear") {
+  //   sortedItems = items.map((item) => item.remove());
+  // }
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
+        ))}
+      </ul>
+      <div className="action">
+        <select
+          value={sortBy}
+          onChange={(e) => {
+            setSortBy(e.target.value);
+            console.log(e.target.value);
+          }}
+        >
+          <option value="input">SORT BY INPUT ORDER</option>
+          <option value="description">SORT By DESCRIPTION</option>
+          <option value="packed">SORT BY PACKED STATUS</option>
+        </select>
+
+        <button onClick={onClearItems}>Clear list</button>
+      </div>
+    </div>
+  );
+}
