f64fa942a442f0ba6c11ff85661da59014eaed2f
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-20 21:24:28 +0200

    Form component created as file, and imported to the parent App component, also imported useState from 'react'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Logo from "./Logo";
+import Form from "./Form";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -42,49 +43,6 @@ export default function App() {
       />
       <Stats items={items} />
     </div>
-  );
-}
-
-function Form({ onAddItems }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!description) return;
-
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-
-    onAddItems(newItem);
-    setDescription("");
-    setQuantity(1);
-  }
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => {
-          console.log(e.target);
-          setDescription(e.target.value);
-        }}
-      />
-      <button onClick={handleSubmit}>Add</button>
-    </form>
   );
 }
 
