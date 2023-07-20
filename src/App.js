c4a9a7bb1e154a01257954c465199cf23f0ee979
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-20 21:16:29 +0200

    Logo component created as file, and imported to the parent App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Logo } from "./Logo";
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -42,10 +43,6 @@ export default function App() {
       <Stats items={items} />
     </div>
   );
-}
-
-function Logo() {
-  return <h1>🌴Far Away💼</h1>;
 }
 
 function Form({ onAddItems }) {
---
+++ src/Logo.js
@@ -0,0 +1,3 @@
+export function Logo() {
+  return <h1>🌴Far Away💼</h1>;
+}
